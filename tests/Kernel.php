ddc3249da3f155098c440a844129458cd125171c
BadPixxel <eshop.bpaquier@gmail.com>
2021-02-05 10:29:45 +0100

    WIP

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -3,7 +3,7 @@
 /*
  *  This file is part of SplashSync Project.
  *
- *  Copyright (C) 2015-2020 Splash Sync  <www.splashsync.com>
+ *  Copyright (C) 2015-2021 Splash Sync  <www.splashsync.com>
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -43,12 +43,12 @@ class Kernel extends BaseKernel
 
         //==============================================================================
         // DOCTRINE CORE
-        $bundles[] = new Doctrine\Bundle\DoctrineBundle\DoctrineBundle();
+//        $bundles[] = new Doctrine\Bundle\DoctrineBundle\DoctrineBundle();
 
         //==============================================================================
         // SPLASH PHP BUNDLE
         $bundles[] = new Splash\Bundle\SplashBundle();
-        $bundles[] = new Splash\Console\ConsoleBundle();
+//        $bundles[] = new Splash\Console\ConsoleBundle();
 
         //==============================================================================
         // SPLASH CONNECTORS BUNDLE
