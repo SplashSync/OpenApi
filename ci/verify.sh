fb2f5d5c881f630f16d85ef7e4e34cd5d6e23690
BadPixxel <eshop.bpaquier@gmail.com>
2023-09-13 16:50:02 +0200

    Update Toolkit

--- ci/verify.sh
+++ ci/verify.sh
@@ -22,12 +22,12 @@ echo '===> Start Docker Stack'
 docker-compose up -d
 
 ################################################################################
-# PHP 7.4
-echo '===> Checks Php 7.4'
-docker-compose exec php-7.4 bash ci/install.sh
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=phpstan
+# PHP 8.1
+echo '===> Checks Php 8.1'
+docker-compose exec php-8.1 bash ci/install.sh
+docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=travis
+docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=csfixer
+docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=phpstan
 
 ################################################################################
 # PHP 8.0
@@ -36,3 +36,13 @@ docker-compose exec php-8.0 bash ci/install.sh
 docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=travis
 docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=csfixer
 docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=phpstan
+
+################################################################################
+# PHP 7.4
+echo '===> Checks Php 7.4'
+docker-compose exec php-7.4 bash ci/install.sh
+docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=travis
+docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=csfixer
+docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=phpstan
+
+
